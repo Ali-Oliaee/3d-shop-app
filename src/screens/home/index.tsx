df802cb084198d3ee92a61669d501172e074efe1
ali <alioliaee31@gmail.com>
2023-08-18 11:57:27 +0330

    add home screen header

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import styles from './styles'
 import BottomTab from '../../components/bottom-tab'
+import BasketIcon from '../../assets/basket'
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>FurnitureCo.</Text>
+        <BasketIcon style={styles.basketIcon}/>
+      </View>
         <BottomTab/>
     </View>
   )
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -4,6 +4,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'green',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: 40,
+        paddingBottom: 10,
+    },
+    logo: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    basketIcon: {
+        transform: [{ scale: 1.1 }]
     }
 })
 
