9f22bd38656568f0cc17d3764edabc3bd58a3d3e
ali <alioliaee31@gmail.com>
2023-08-21 13:19:38 +0330

    lint screens

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,53 +1,49 @@
-import React from 'react'
-import { ScrollView, Text, View } from 'react-native'
-import styles from './styles'
-import BottomTab from '../../components/bottom-tab'
-import BasketIcon from '../../assets/basket'
-import SearchInput from '../../components/search-input'
-import CategoriesContainer from '../../components/categories-container'
-import ProductCard from '../../components/product-card'
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+import styles from './styles';
+import BasketIcon from '../../assets/basket';
+import BottomTab from '../../components/bottom-tab';
+import CategoriesContainer from '../../components/categories-container';
+import ProductCard from '../../components/product-card';
+import SearchInput from '../../components/search-input';
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.logo}>Shoe Store</Text>
-        <BasketIcon style={styles.basketIcon}/>
+        <BasketIcon style={styles.basketIcon} />
       </View>
-      <SearchInput/>
-      <CategoriesContainer/>
+      <SearchInput />
+      <CategoriesContainer />
       <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={styles.productsList}
-      contentContainerStyle={{
-        flex: 1,
-        width: '100%',
-        paddingBottom: 40,
-        paddingTop: 60,
-        paddingHorizontal: 10,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-      }}
-      >
+        showsVerticalScrollIndicator={false}
+        style={styles.productsList}
+        contentContainerStyle={{
+          flex: 1,
+          width: '100%',
+          paddingBottom: 40,
+          paddingTop: 60,
+          paddingHorizontal: 10,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}>
         <ProductCard
           title="Nike Sport Edition"
           price="$50.18"
           image={require('../../assets/nikeShoe.png')}
           onDetails={() => navigation.navigate('Product1')}
         />
-          <ProductCard
-          title="Adidas Hill"
-          price="$12.39"
-          image={require('../../assets/sport.png')}
-        />
-         <ProductCard
+        <ProductCard title="Adidas Hill" price="$12.39" image={require('../../assets/sport.png')} />
+        <ProductCard
           title="Walking Shoe"
           price="$30.99"
           image={require('../../assets/nikeWalk.png')}
           onDetails={() => navigation.navigate('Product2')}
         />
-          <ProductCard
+        <ProductCard
           title="Sport Shoe"
           price="$42.49"
           image={require('../../assets/blackShoe.png')}
@@ -59,15 +55,15 @@ const HomeScreen = ({navigation}) => {
           hasShadow={false}
         />
         <ProductCard
-        title="Gucci Sandal"
-        price="$33.00"
-        image={require('../../assets/sandal.png')}
-        hasShadow={false}
-      />
+          title="Gucci Sandal"
+          price="$33.00"
+          image={require('../../assets/sandal.png')}
+          hasShadow={false}
+        />
       </ScrollView>
-      <BottomTab navigation={navigation}/>
+      <BottomTab navigation={navigation} />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- src/screens/product2/index.tsx
+++ src/screens/product2/index.tsx
@@ -1,48 +1,48 @@
-import React, { Suspense, useEffect, useRef, useState } from 'react'
-import ProductLayout from '../../layouts/product-screen-layout'
-import { Canvas } from '@react-three/fiber'
-import NikeShoeWalk from '../../models/shoe2'
 import Slider from '@react-native-community/slider';
-import { ActivityIndicator, View } from 'react-native'
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
-const Product2Screen = ({navigation}) => {
-  const [isTouch, setIsTouch] = useState(false)
-  const [count, setCount] = useState<number>(1)
-  const [rotation, setRotation] = useState<number[]>([0, 0, 0])
-  const [horizontalRotate, setHorizontalRotate] = useState<number>(0)
-  const [verticalRotate, setVerticalRotate] = useState<number>(0)
+import ProductLayout from '../../layouts/product-screen-layout';
+import NikeShoeWalk from '../../models/shoe2';
+
+const Product2Screen = ({ navigation }) => {
+  const [isTouch, setIsTouch] = useState(false);
+  const [count, setCount] = useState<number>(1);
+  const [rotation, setRotation] = useState<number[]>([0, 0, 0]);
+  const [horizontalRotate, setHorizontalRotate] = useState<number>(0);
+  const [verticalRotate, setVerticalRotate] = useState<number>(0);
 
   useEffect(() => {
-    setRotation([horizontalRotate,verticalRotate,0])
-  }, [horizontalRotate, verticalRotate])
-  
+    setRotation([horizontalRotate, verticalRotate, 0]);
+  }, [horizontalRotate, verticalRotate]);
 
   return (
     <ProductLayout
-      title='Walking Shoe'
+      title="Walking Shoe"
       description="The Nike Sport Edition is a collection of shoes designed for athletes and sports enthusiasts. With a focus on performance and style, these shoes are built to provide comfort, support, and durability during various sporting activities. Whether you're a runner, a gym-goer, or a trail enthusiast, Nike Sport Edition offers a range of options to suit your needs."
       price="$30.99"
       count={count}
-      onBackPress={()=>navigation.navigate('Home')}
-      setCount={setCount}
-    >
-        <Suspense fallback={<View style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center'
-          }}>
-          <ActivityIndicator
-          size='large'
-          color='#000'
-          />
-        </View>}>
-      <Canvas>
-        <ambientLight/>
-          <NikeShoeWalk rotation={rotation} isTouch={isTouch}/>
-      </Canvas>
-        </Suspense>
+      onBackPress={() => navigation.navigate('Home')}
+      setCount={setCount}>
+      <Suspense
+        fallback={
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <ActivityIndicator size="large" color="#000" />
+          </View>
+        }>
+        <Canvas>
+          <ambientLight />
+          <NikeShoeWalk rotation={rotation} isTouch={isTouch} />
+        </Canvas>
+      </Suspense>
       <Slider
-        style={{width: '100%', height: 40}}
+        style={{ width: '100%', height: 40 }}
         minimumValue={0}
         maximumValue={6}
         minimumTrackTintColor="#000000"
@@ -53,7 +53,14 @@ const Product2Screen = ({navigation}) => {
         onTouchStart={() => setIsTouch(true)}
       />
       <Slider
-        style={{width: '100%', height: 40,right: -160, top: 250, position:'absolute', transform:[{rotate: '90deg'}]}}
+        style={{
+          width: '100%',
+          height: 40,
+          right: -160,
+          top: 250,
+          position: 'absolute',
+          transform: [{ rotate: '90deg' }],
+        }}
         minimumValue={0}
         maximumValue={6}
         minimumTrackTintColor="#000000"
@@ -62,9 +69,9 @@ const Product2Screen = ({navigation}) => {
         value={verticalRotate}
         onTouchEnd={() => setIsTouch(false)}
         onTouchStart={() => setIsTouch(true)}
-        />
+      />
     </ProductLayout>
-  )
-}
+  );
+};
 
-export default Product2Screen
+export default Product2Screen;
