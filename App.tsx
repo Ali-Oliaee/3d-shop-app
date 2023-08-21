873a3fc6a3ab758be7e6f5eca8ed4fafc7400fef
ali <alioliaee31@gmail.com>
2023-08-21 13:17:02 +0330

    lint assets

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './src/screens/home';
 import Product1Screen from './src/screens/product1';
 import Product2Screen from './src/screens/product2';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfileScreen from './src/screens/profile';
 
 const Stack = createNativeStackNavigator();
@@ -10,11 +11,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Product1" component={Product1Screen} />
         <Stack.Screen name="Product2" component={Product2Screen} />
-        <Stack.Screen name='Profile' component={ProfileScreen}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
